eb819a64a01410f8bcfd3feaacb7df573243dd19
Alvaro Silva <Alvaro Silva>
2016-10-15 15:53:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# react-timer
+A timer and countdown app done in React
+
+You can see it running at: http://fathomless-tundra-82042.herokuapp.com/#/?_k=tmhrcm
+
+If you have any questions reach out at alvaro.augustho@mgmail.com
---
+++ app/app.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {
+    Route, Router, IndexRoute, hashHistory
+} = require('react-router');
+
+// Load Foundation
+$(document).foundation();
+
+//App style
+require('style!css!sass!ApplicationStyles')
+
+ReactDOM.render(
+        <p>Boilerplate project</p>
+        document.getElementById('app')
+    )
