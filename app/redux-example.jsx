b3d5d4147806ca7e3055b81b7259d6dd931df78a
Alvaro Silva <Alvaro Silva>
2016-10-15 17:45:49 +0200

    Add support for change name action

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -3,9 +3,29 @@ var redux = require('redux');
 console.log('Starting redux example');
 
 var reducer = (state = {name: 'Anonymous'}, action) => {
+    
+    switch (action.type) {
+        case 'CHANGE_NAME':
+            return {
+                ...state,
+                name: action.name
+            };
+        default:
+            return state;
+    }
+    
     return state;
 };
 var store = redux.createStore(reducer);
+
+var currentState = store.getState();
+console.log('currentState', currentState);
+
+var action = {
+    type: 'CHANGE_NAME',
+    name: 'Alvaro'
+};
+store.dispatch(action);
 
 var currentState = store.getState();
 console.log('currentState', currentState);
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -6,11 +6,29 @@ var stateDefault = {
     searchText: '',
     showCompleted: false,
     todos: []
-}
+};
 var reducer = (state = stateDefault, action) => {
-    return state;
+    
+    switch(action.type) {
+        case 'CHANGE_SEARCH_TEXT':
+            return {
+                ...state,
+                searchText: action.searchText
+            };
+        default:
+            return state;
+    }
+    
 };
 
 var store = redux.createStore(reducer);
+
+console.log('currentState', store.getState());
+
+var action = {
+    type: 'CHANGE_SEARCH_TEXT',
+    searchText: 'searching this text...'
+};
+store.dispatch(action);
 
 console.log('currentState', store.getState());
