b5d52d639b0716364c82b0bcb837f1dd1c84353c
Alvaro Silva <Alvaro Silva>
2016-10-16 20:33:43 +0200

    Refactor file to create separate files for actions, reducers and store

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,117 +1,12 @@
 var redux = require('redux');
-var axios = require('axios');
 
 console.log('Starting redux example');
 
-//  Name reducer and generators
-// --------------------------------
-var nameReducer = (state = 'Anonymous', action) => {
-    switch (action.type) {
-        case 'CHANGE_NAME':
-            return action.name;
-        default:
-            return state;
-    }
-};
+var actions = require('./actions/index');
+var store = require('./store/configureStore').configure();
 
-var changeName = (name) => {
-    return {
-        type: 'CHANGE_NAME',
-        name
-    }
-};
 
-//  Hobbies reducer and generators
-// --------------------------------
-var nextHobbyId = 1;
-var hobbiesReducer = (state = [], action) => {
-    switch (action.type) {
-        case 'ADD_HOBBY':
-            return [
-                ...state,
-                {   
-                    id: nextHobbyId++,
-                    hobby: action.hobby
-                }
-                ];
-        case 'REMOVE_HOBBY':
-             return state.filter((hobby) => hobby.id !== action.id)
-        default:
-            return state;
-    }  
-};
 
-var addHobby = (hobby) => {
-    return {
-        type: 'ADD_HOBBY',
-        hobby
-    }  
-};
-
-var removeHobby = (id) => {
-    return {
-        type: 'REMOVE_HOBBY',
-        id
-    }  
-};
-
-// Map reducer and action generators
-// ---------------------------------
-
-var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
-    
-    switch(action.type) {
-        
-        case 'START_LOCATION_FETCH':
-            return {
-                isFetching: true,
-                url: undefined
-            };
-        case 'COMPLETE_LOCATION_FETCH':
-            return {
-                isFetching: false,
-                url: action.url
-            };
-        default:
-            return state;
-    }
-    
-};
-
-var startLocationFetch = () => {
-    return {
-        type: 'START_LOCATION_FETCH'
-    }
-};
-
-var completeLocationFetch = (url) => {
-    return {
-        type: 'COMPLETE_LOCATION_FETCH',
-        url
-    }
-};
-
-var fetchLocation = () => {
-    
-    store.dispatch(startLocationFetch());
-    
-    axios.get('http://ipinfo.io').then(function(res) {
-        var loc = res.data.loc;
-        var baseUrl = 'http://maps.google.com?q=';
-        
-        store.dispatch(completeLocationFetch(baseUrl + loc));
-    });
-};
-
-var reducer = redux.combineReducers({
-    name: nameReducer,
-    hobbies: hobbiesReducer,
-    map: mapReducer
-});
-
-var store = redux.createStore(reducer, redux.compose(
-    window.devToolsExtension ? window.devToolsExtension() : f => f
-));
 
 // Subscribe to changes
 var unsubscribe = store.subscribe(() => {
@@ -130,14 +25,14 @@ var unsubscribe = store.subscribe(() => {
 var currentState = store.getState();
 console.log('currentState', currentState);
 
-fetchLocation();
+store.dispatch(actions.fetchLocation());
 
-store.dispatch(changeName('Alvaro'));
+store.dispatch(actions.changeName('Alvaro'));
 
-store.dispatch(addHobby('Walking'));
+store.dispatch(actions.addHobby('Walking'));
 
-store.dispatch(addHobby('Running'));
+store.dispatch(actions.addHobby('Running'));
 
-store.dispatch(removeHobby(2));
+store.dispatch(actions.removeHobby(2));
 
-store.dispatch(changeName('Emily'));
+store.dispatch(actions.changeName('Emily'));
